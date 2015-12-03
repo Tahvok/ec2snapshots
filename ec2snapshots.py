9981bdf12a959114ac4ae9d9387c4bd688f561ae
Albert Mikaelyan <albert@websplanet.com>
2015-12-03 20:18:40 +0200

    rewrite using classes

--- ec2snapshots.py
+++ ec2snapshots.py
@@ -5,54 +5,97 @@ import argparse
 from boto3.session import Session
 
 
-def run_backup(volumes, ec2):
-    # Loop through all volumes
-    for volume in volumes['Volumes']:
+class Volumes(object):
 
-        # Set volume name to volume id
-        volume_name = volume['VolumeId']
+    def __init__(self, ec2, check):
+        self.check = check
 
-        # Check if Tags exist
-        if 'Tags' in volume:
+        self.ec2 = ec2
 
-            # Do backups flag
-            do_backup = False
+        self.volumes = ec2.describe_volumes()
 
-            # Loop all tags
-            for tag in volume['Tags']:
+    def get_volumes(self):
+        return self.volumes
 
-                # Assign correct volume name if we found one
-                if 'Name' in tag['Key']:
-                    volume_name = tag['Value']
+    def __get_backup_volumes(self, backup_word, volumes=None):
+        if volumes is None:
+            volumes = self.volumes
 
-                # Lowercase the Value
-                value = tag['Value'].lower()
+        backup_volumes = {}
 
-                # Check if Value contains the backup word
-                if args.word in value:
-                    # set to True as the word was found
-                    do_backup = True
-                    break
+        # Loop through all volumes
+        for volume in volumes['Volumes']:
 
-            # Only print the message if --check is set
-            if do_backup and args.check:
+            # Set volume name to volume id
+            volume_name = volume['VolumeId']
+
+            volume_id = volume['VolumeId']
+
+            # Check if Tags exist
+            if 'Tags' in volume:
+
+                # Do backups flag
+                do_backup = False
+
+                # Loop all tags
+                for tag in volume['Tags']:
+
+                    # Assign correct volume name if we found one
+                    if 'Name' in tag['Key']:
+                        volume_name = tag['Value']
+
+                    # Lowercase the Value
+                    value = tag['Value'].lower()
+
+                    # Check if Value contains the backup word
+                    if backup_word in value:
+                        # set to True as the word was found
+                        do_backup = True
+                        backup_volumes[volume_id] = volume_name
+
+                        # Print message if --check is set
+                        if self.check:
+                            print(
+                                    "Check " + volume_name + ": '" +
+                                    backup_word +
+                                    "' was found in tag, will create backup."
+                            )
+                        break
+
+                # Print the message if --check is set and no 'word' found
+                if self.check and not do_backup:
+                    print(
+                            'Check ' + volume_name + ': \'' +
+                            backup_word +
+                            '\' was NOT found in the tags, ' +
+                            'will NOT create backup.'
+                    )
+
+            # If no tags were found and --check is set
+            elif self.check:
                 print(
-                        "Check " + volume_name + ": '" +
-                        args.word + "' was found in tag, will create backup."
+                        'Check ' + volume_name +
+                        ': no tags were found. Will NOT create backup.'
                 )
 
-            # Create the snapshot otherwise
-            elif do_backup:
+        return backup_volumes
+
+    def run_backup(self, backup_word, backup_volumes=None):
+        if backup_volumes is None:
+            backup_volumes = self.__get_backup_volumes(backup_word)
+
+        if not self.check:
+            for volume_id, volume_name in backup_volumes.iteritems():
                 # Create snapshot
-                result = ec2.create_snapshot(
-                        VolumeId=volume['VolumeId'],
+                result = self.ec2.create_snapshot(
+                        VolumeId=volume_id,
                         Description='Scheduled Snapshot [' +
-                                    volume['VolumeId'] +
+                                    volume_id +
                                     '] - ec2backup'
                 )
 
                 # Create tags
-                ec2.create_tags(
+                self.ec2.create_tags(
                         Resources=[
                             result['SnapshotId'],
                         ],
@@ -65,20 +108,23 @@ def run_backup(volumes, ec2):
                         ]
                 )
 
-            # Print the message if --check is set and no 'word' found
-            elif args.check:
-                print(
-                        'Check ' + volume_name + ': \'' +
-                        args.word +
-                        '\' was NOT found in the tags, will NOT create backup.'
-                )
 
-        # If no tags were found and --check is set
-        elif args.check:
-            print(
-                    'Check ' + volume_name +
-                    ': no tags were found. Will NOT create backup.'
-            )
+class Snapshots(object):
+    def __init__(self, ec2, check):
+        self.volumes = Volumes(ec2, check)
+
+    def print_volumes(self):
+        return self.volumes.get_volumes()
+
+
+# def delete_old_backup(ec2):
+    # snapshots = ec2.describe_snapshots()
+
+    # print(snapshots)
+#     volumes = Ec2Volumes(ec2)
+
+#     print(volumes.backup_volumes)
+
 
 if '__main__' == __name__:
     # Parse arguments
@@ -99,8 +145,6 @@ if '__main__' == __name__:
     # Create ec2 object with given region
     EC2 = session.client('ec2', region_name=args.region)
 
-    # Get all volumes from ec2
-    VOLUMES = EC2.describe_volumes()
+    my_volumes = Volumes(EC2, args.check)
 
-    run_backup(VOLUMES, EC2)
-
+    my_volumes.run_backup(args.word)
